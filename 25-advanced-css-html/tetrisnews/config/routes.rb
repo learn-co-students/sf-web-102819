2ff6f4a1a4373d5a3e90ae9ab3e3485590886ecb
Levi Miller <paullevimiller@gmail.com>
2019-12-04 09:48:16 -0800

    Update Lecture 25 with Sinatra example

--- 25-advanced-css-html/tetrisnews/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :players
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/', to: 'pages#home', as: 'home'
-end
