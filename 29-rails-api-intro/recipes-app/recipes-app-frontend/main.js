fd6efbdef0a15785e2086167936854ee77c748b2
Levi Miller <paullevimiller@gmail.com>
2019-12-12 11:59:18 -0800

    Lecture 29 live code

--- 29-rails-api-intro/recipes-app/recipes-app-frontend/main.js
+++ 29-rails-api-intro/recipes-app/recipes-app-frontend/main.js
@@ -1,11 +1,19 @@
-console.log("Hello!");
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  fetch('http://localhost:3000/recipes')
-    .then(response => response.json())
-    .then(json => console.log(json));
-});
+let ulElement; 
 
 function renderRecipes(json) {
-  
+  return json.map(recipe => {
+    let listElement = document.createElement("li");
+    listElement.innerText = recipe.name;
+    ulElement.append(listElement);
+  });
 };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  ulElement = document.getElementById("recipes");
+  fetch('http://localhost:3000/recipes')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      renderRecipes(json);
+    });
+});
