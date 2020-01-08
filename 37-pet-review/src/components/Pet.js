49d4bb4710b86df2dea4e5a6e677d1bb3d3b2952
Levi Miller <paullevimiller@gmail.com>
2020-01-08 10:18:53 -0800

    Work through Lecture 37

--- 37-pet-review/src/components/Pet.js
+++ 37-pet-review/src/components/Pet.js
@@ -1,26 +1,40 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  handleAdoptClick = (event) => this.props.onAdoptPet(this.props.pet.id)
+
   render() {
+
+    let buttonConfig;
+    if (this.props.pet.isAdopted) {
+      buttonConfig = <div className="extra content">
+            <button className="ui disabled button" >Adopt pet</button>
+            <button className="ui primary button">Already adopted</button>
+        </div>
+    } else {
+      buttonConfig = <div className="extra content">
+            <button className="ui primary button" onClick={this.handleAdoptClick}>Adopt pet</button>
+            <button className="ui disabled button">Already adopted</button>
+        </div>
+    }; 
+
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
-        </div>
+        {buttonConfig}
       </div>
     )
   }
