214ab9e781f0153defabf01e96ea4c2ffb7501bd
Levi Miller <paullevimiller@gmail.com>
2020-01-09 10:37:32 -0800

    Update Lecture 38 solution

--- 38-lifecycle-methods/src/lifcycle/GrandChild.js
+++ 38-lifecycle-methods/src/lifcycle/GrandChild.js
@@ -4,16 +4,33 @@ class GrandChild extends Component {
   constructor() {
     super();
     console.log("%c GrandChild Constructor", "color: green");
+    this.state = {
+      buttonText: "Grandchild"
+    };
   }
 
   componentDidMount() {
     console.log("%c GrandChild Did Mount", "color: green");
   }
 
+  componentDidUpdate() {
+    console.log("%c GrandChild Did Update", "color: green");
+  };
+
+  handleClick = () => {
+    this.setState(previousState => {
+      return {buttonText: previousState.buttonText + "!"}
+    })
+  };
+
   render() {
     console.log("%c GrandChild Render", "color: green");
 
-    return <div className="box">GrandChild</div>;
+    return (
+      <div className="box">
+        <button onClick={this.handleClick}>{this.state.buttonText}</button>
+      </div>
+    );
   }
 }
 
--- 38-lifecycle-methods/src/lifcycle/Parent.js
+++ 38-lifecycle-methods/src/lifcycle/Parent.js
@@ -5,18 +5,31 @@ class Parent extends Component {
   constructor() {
     super();
     console.log("%c Parent Constructor", "color: blue");
+    this.state = {
+      buttonText: "Parent"
+    };
   }
 
   componentDidMount() {
     console.log("%c Parent Did Mount", "color: blue");
   }
 
+  componentDidUpdate() {
+    console.log("%c Parent Did Update", "color: blue");
+  };
+
+  handleClick = () => {
+    this.setState(previousState => {
+      return {buttonText: previousState.buttonText + "."}
+    });
+  };
+
   render() {
     console.log("%c Parent Render", "color: blue");
 
     return (
       <div className="box">
-        Parent
+        <button onClick={this.handleClick}>{this.state.buttonText}</ button>
         <Child />
       </div>
     );
